31a81194dc1f51ba161a6253f73547aac05e8dd7
harutiro <harutiro2027@outlook.jp>
2024-01-06 20:18:37 +0900

    add: ボトムナビゲーションの作成

--- app/src/main/java/net/harutiro/nationalweather/MainActivity.kt
+++ app/src/main/java/net/harutiro/nationalweather/MainActivity.kt
@@ -4,12 +4,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import net.harutiro.nationalweather.core.entities.BottomNavigationItem
+import net.harutiro.nationalweather.features.favorite.page.FavoritePage
+import net.harutiro.nationalweather.features.home.page.HomePage
 import net.harutiro.nationalweather.ui.theme.NationalWeatherTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +44,99 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    App()
                 }
             }
         }
     }
 }
 
+
+
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun App(){
+    var navController = rememberNavController()
+
+    val items = listOf(
+        BottomNavigationItem(
+            title = stringResource(id = R.string.home),
+            selectedIcon = Icons.Filled.Home,
+            unselectedIcon = Icons.Filled.Home,
+            hasNews = true,
+            badgeCount = 2
+        ),
+        BottomNavigationItem(
+            title = stringResource(id = R.string.favorite),
+            selectedIcon = Icons.Filled.Favorite,
+            unselectedIcon = Icons.Filled.Favorite,
+            hasNews = false,
+            badgeCount = null
+        ),
     )
+
+    // 画面遷移の状態を扱う変数を追加
+    var selectedItemIndex by rememberSaveable {
+        mutableStateOf(0)
+    }
+
+    Scaffold(
+        bottomBar = {
+            NavigationBar {
+                items.forEachIndexed { index ,screen ->
+                    NavigationBarItem(
+                        selected = selectedItemIndex == index,
+                        onClick = {
+                            selectedItemIndex = index
+                            navController.navigate(screen.title)
+                        },
+                        icon = {
+                            BadgedBox(
+                                badge = {
+                                    if(screen.badgeCount != null) {
+                                        Badge {
+                                            Text(text = screen.badgeCount.toString())
+                                        }
+                                    } else if(screen.hasNews) {
+                                        Badge()
+                                    }
+                                }
+                            ) {
+                                Icon(
+                                    imageVector = if (index == selectedItemIndex) {
+                                        screen.selectedIcon
+                                    } else screen.unselectedIcon,
+                                    contentDescription = screen.title
+                                )
+                            }
+                        },
+                        label = { Text(screen.title) },
+                    )
+                }
+            }
+        }
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = items[selectedItemIndex].title,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            composable("home") {
+                HomePage()
+            }
+            composable("favorite") {
+                FavoritePage()
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     NationalWeatherTheme {
-        Greeting("Android")
+        App()
     }
 }
