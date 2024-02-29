3a8222fad092a2a707a6c841963590533819f47f
harutiro <harutiro2027@outlook.jp>
2024-03-01 03:57:42 +0900

    add: Dateの表記の仕方を統一させた。

--- app/src/main/java/net/harutiro/nationalweather/core/utils/DateUtils.kt
+++ app/src/main/java/net/harutiro/nationalweather/core/utils/DateUtils.kt
@@ -11,5 +11,12 @@ class DateUtils {
             val format = SimpleDateFormat("MM月dd日 (E)" , java.util.Locale.JAPAN)
             return format.format(date)
         }
+
+        fun apiDateToJapaneseNotation(dateString: String): String {
+            val apiFormat = SimpleDateFormat("yyyy-MM-dd", java.util.Locale.JAPAN)
+            val date = apiFormat.parse(dateString) ?: Date()
+            val japanNotationFormat = SimpleDateFormat("MM月dd日 (E)" , java.util.Locale.JAPAN)
+            return japanNotationFormat.format(date)
+        }
     }
 }
