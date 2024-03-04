0b15537fe408af83712aad077245bb69081e0ba7
harutiro <harutiro2027@outlook.jp>
2024-03-04 20:41:57 +0900

    add: お気に入りに何もないときにその旨を伝えるテキストを追加

--- app/src/main/java/net/harutiro/nationalweather/core/presenter/favorite/page/FavoritePage.kt
+++ app/src/main/java/net/harutiro/nationalweather/core/presenter/favorite/page/FavoritePage.kt
@@ -2,24 +2,32 @@ package net.harutiro.nationalweather.core.presenter.favorite.page
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
+import net.harutiro.nationalweather.R
 import net.harutiro.nationalweather.core.presenter.favorite.viewModel.FavoriteViewModel
 import net.harutiro.nationalweather.features.Weather.entities.CityId
 
@@ -62,6 +70,24 @@ fun FavoritePage(viewModel: FavoriteViewModel = viewModel()) {
                             }
                         }
                     }
+                )
+            }
+        }
+
+        if (viewModel.weatherList.isEmpty()) {
+            Column(
+                modifier = Modifier.fillMaxSize().padding(20.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    textAlign = TextAlign.Center,
+                    text = stringResource(id = R.string.empty_text),
+                )
+                Text(
+                    textAlign = TextAlign.Center,
+                    style = MaterialTheme.typography.titleLarge,
+                    text ="(人；´Д｀)ｺﾞﾒﾝﾈ"
                 )
             }
         }
