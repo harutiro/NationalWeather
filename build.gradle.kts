7e82e5336364a6058116aeb2bff3d1c8dd4cdb0e
harutiro <harutiro2027@outlook.jp>
2025-04-20 03:42:40 +0900

    update: gradle関係のアップデート

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
-    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
+    id("com.android.application") version "8.9.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.20" apply false
+    id("com.google.devtools.ksp") version "2.1.20-2.0.0" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.2"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.20" apply false // ★追加
 }
